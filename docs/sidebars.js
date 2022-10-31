7a93e4aee31f335e568bdf523b6ea18d47aaecad
urob <978080+urob@users.noreply.github.com>
2022-10-31 12:24:19 -0400

    Swapper

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -31,6 +31,7 @@ module.exports = {
       "behaviors/key-toggle",
       "behaviors/sticky-key",
       "behaviors/sticky-layer",
+      "behaviors/tri-state",
       "behaviors/tap-dance",
       "behaviors/caps-word",
       "behaviors/key-repeat",
